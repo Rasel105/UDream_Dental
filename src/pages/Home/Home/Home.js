6bacba1f7908c40da0e095016b09813bd2f26e44
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-04-17 02:29:25 +0600

    banner section 1/3 complete

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
      return (
           <div>
-               <h2>This is Banner</h2>
+               <Banner />
           </div>
      );
 };
