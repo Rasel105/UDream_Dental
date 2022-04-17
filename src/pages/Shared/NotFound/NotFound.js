3e840cfd2948f863917c5594ac5d411d1777f4b8
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-04-18 01:27:40 +0600

    404 not found page setup

--- src/pages/Shared/NotFound/NotFound.js
+++ src/pages/Shared/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import notfound from '../../../images/Not-Found/NotFound.jpg'
 const NotFound = () => {
      return (
-          <div>
-               <h2>404 | Not Found</h2>
+          <div className='d-flex justify-content-center align-items-center'>
+               <h2 className='display-1 mx-4 text-danger'>404 | Not Found</h2>
+               <img className='img-fluid w-50 my-5 rounded' style={{ height: '500px' }} src={notfound} alt="" />
           </div>
      );
 };
