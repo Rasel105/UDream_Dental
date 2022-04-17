8b7ed37f4e2f1776f9fb83fb4b223f1836a46c34
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-04-18 02:12:21 +0600

    custom link added

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import personalPhoto from '../../images/Personal/Rasel.jpg'
+import './About.css'
+
 const About = () => {
      return (
           <div>
                <h2 className='text-center text-info mt-2'>About Myself</h2>
                <div className='d-flex justify-content-center align-items-center mt-4 mb-5 '>
-                    <Card className='rounded' style={{ width: '20rem' }}>
+                    <Card className='rounded outercss' style={{ width: '25rem' }}>
                          <img className='rounded' src={personalPhoto} alt="" />
                          <Card.Body>
-                              <Card.Title className='text-center mb-3'>Md. Azaddul Islam Rasel</Card.Title>
+                              <Card.Title className='text-center mb-3 text-info' style={{ fontSize: "20px" }}>Md. Azaddul Islam Rasel</Card.Title>
                               <Card.Text>
+                                   <span className='text-primary display-4'>Goal:</span>
                                    The profession of software engineers is really cool and full of challenges, the aim to build or develop the applications is like the challenges that every software developer faced during his/her field.
 
-                                   Increasing my performance metrics are concerned with how efficient you are at work. It has to do with ensuring customer satisfaction, delivering projects within deadlines and helping companies reduce costs. It basically helps you build up your reputation in the workplace and makes it that much easier for you to achieve long-term goals like promotions.
+                                   Increasing my performance metrics are concerned with how efficient you are at work.
 
                               </Card.Text>
                          </Card.Body>
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
      const [user] = useAuthState(auth);
@@ -20,17 +21,17 @@ const Header = () => {
                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                     <Container>
                          <Navbar.Brand as={Link} to='/'>
-                              <h3>UDreamDental</h3>
+                              <h3 style={{ color: "#45B8AC" }}>UDreamDental</h3>
                               {/* <img style={{ width: "150px", height: '40px' }} src={logo} alt="" /> */}
                          </Navbar.Brand>
                          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                          <Navbar.Collapse id="responsive-navbar-nav">
                               <Nav className="me-auto">
-                                   <Nav.Link className="fs-5" as={Link} to="/service">Services</Nav.Link>
-                                   <Nav.Link className="fs-5" as={Link} to="/blog">Blogs</Nav.Link>
+                                   <Nav.Link className="fs-5" as={CustomLink} to="/service">Services</Nav.Link>
+                                   <Nav.Link className="fs-5" as={CustomLink} to="/blog">Blogs</Nav.Link>
                               </Nav>
                               <Nav>
-                                   <Nav.Link className="fs-5" as={Link} to='/aboutme'>
+                                   <Nav.Link className="fs-5" as={CustomLink} to='/aboutme'>
                                         About Me
                                    </Nav.Link>
                                    <Nav.Link>
@@ -38,7 +39,7 @@ const Header = () => {
                                              user ?
                                                   <p className="fs-5" onClick={hanldeSignOut}>Logout</p>
                                                   :
-                                                  <Link style={{ textDecoration: "none", color: "gray" }} to='/login' className="fs-5" >Login</Link>
+                                                  <CustomLink to='/login' className="fs-5">Login</CustomLink >
                                         }
                                    </Nav.Link>
                               </Nav>
